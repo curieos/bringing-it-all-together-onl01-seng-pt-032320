d56b3c6c22304298ceed31f4b23ffbe321d22963
Caroline Longnecker <cvrieous@gmail.com>
2020-06-10 18:45:56 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  attr_reader :id
+  attr_accessor :name, :breed
+  
+  def initialize(id: nil, name: String, breed: String)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def create_table
+    
+  end
+end
