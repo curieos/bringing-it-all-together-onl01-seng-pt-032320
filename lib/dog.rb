45f9f610e71ec1dad70b565cfd2717b6a4efa878
Caroline Longnecker <cvrieous@gmail.com>
2020-06-11 02:19:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,6 +66,6 @@ class Dog
     
     rawDog = DB[:conn].execute(sql, name, breed)
     
-    Dog.new(id: rawDog[0] ? rawDog[0][0] : nil, name: rawDog[0] ? rawDog[0][1] : name)
+    Dog.new(id: rawDog[0][0] ? rawDog[0][0] : nil, name: rawDog[0] ? rawDog[0][1] : name)
   end
 end
